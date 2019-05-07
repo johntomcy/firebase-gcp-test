54fdc5277cd0c8563d0c77ab4c2029c52fc75a38
Tomcy John <tomcy.john@emirates.com>
2019-05-07 21:45:40 +0400

    another test

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React and Firebase
+            Learn React and Firebase by Tomcy
           </a>
         </header>
       </div>
